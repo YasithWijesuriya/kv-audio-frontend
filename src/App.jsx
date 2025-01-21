4c05b60387227cca4307f8fafdbc50d50b0bbe5e
yasith wijesuriya <yasithruchiranga95@gmail.com>
2025-01-21 21:03:38 +0530

    updated frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
 import './App.css'
-import  ProductCard  from './components/productCard.jsx'
+import { BsGraphDown } from "react-icons/bs";
+import { PiBookmarkSimpleBold } from "react-icons/pi";
+import { LuSpeaker } from "react-icons/lu";
+import { FaRegUser } from "react-icons/fa";
+
+
 
 function App() {
 
-return(
-  <div>
-  <ProductCard name="Coca Cola"  price="400/-" description="It is a cocee" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxPwagsrUPMvuj-3EzzLmaKEd4LqvOym2HobFxeSh9iB3jRcxmTNjazwwj9dC-WddyCrc&usqp=CAU"/>
+  return (
+    <div className='w-full h-screen flex'>
+      <div className='w-[400px] h-full bg-gray-200'>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+        <BsGraphDown/>
+          Dashboard 
+          </button>
+          <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+            <PiBookmarkSimpleBold/>
+            Bookings
+            </button>
+            <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+              <LuSpeaker/>
+              Items
+              </button>
+              <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'>
+                <FaRegUser/>
+                Users
+                </button>
+              
+      </div>
+      <div className='w-full bg-red-950'>
 
-  <ProductCard name="Pepsi"  price="300/-" description="it is a drink" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzSIIxN976Bcr6f8QU7mUM_f5DDbR-goMyMQ&s"/>
 
-  <ProductCard name="poison"  price="free/-" description="It is a poison bottle" img="https://greendroprecycling.com/wp-content/uploads/2017/04/GreenDrop_Station_Aluminum_Can_3.jpg"/>
+      </div>
 
-  </div>
-)
+    </div>
+  )
 
 }
 
