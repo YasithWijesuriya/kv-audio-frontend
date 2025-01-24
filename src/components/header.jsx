bcee878f79edfad4bfeb628a8967e8a70fc20a90
yasith wijesuriya <yasithruchiranga95@gmail.com>
2025-01-24 22:37:24 +0530

    updated header.jsx ✔

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,8 +5,8 @@ export default function Header(){
         <header className = 'w-full h-[100px] shadow-xl flex justify-center items-center relative'>
             <img src="/logo.png" alt="Logo" className="w-[100px] h-[100px] object-cover absolute left-1 border-[3px] rounded-full shadow"/>
             <Link to="/" className="text-[25px] font-bold m-3">Home</Link>
-            <Link to="/" className="text-[25px] font-bold m-3">Contact</Link>
-            <Link to="/" className="text-[25px] font-bold m-3">Gallery</Link>
+            <Link to="/contact" className="text-[25px] font-bold m-3">Contact</Link>
+            <Link to="/gallery" className="text-[25px] font-bold m-3">Gallery</Link>
 
         </header>
     
