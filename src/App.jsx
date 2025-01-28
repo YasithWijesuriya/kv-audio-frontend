95a91f66c6caf4cd8d6b1c59789979ba17b4eb7e
yasith wijesuriya <yasithruchiranga95@gmail.com>
2025-01-28 19:46:51 +0530

    create testing.jsx file in components and plug

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route,Routes } from 'react-router-dom';
 import './App.css'
 import AdminPage from './pages/admin/Adminpage';
 import HomePage from './pages/home/homePage';
+import Testing from './components/testing';
 
 
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes path="/*">
+        <Route path="/testing" element = {<Testing/>}/>
         <Route path ="/admin/*" element={<AdminPage/>}/>
         {/*'admin/*' මෙසේ යොදා ඇත්තෙ admin ලෙස url එකේ සදහන් කර එතනින් පස්සෙ ගහන
         ඔනෑම අදාල නැති url වලින් එන අයට මුලින්ම admin page එක පෙන්නීම */}
