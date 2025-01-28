7f800944541346139d30cf8cb270dc0e7f10222e
yasith wijesuriya <yasithruchiranga95@gmail.com>
2025-01-28 23:19:29 +0530

    login.jsx updated✔

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import AdminPage from './pages/admin/Adminpage';
 import HomePage from './pages/home/homePage';
 import Testing from './components/testing';
+import LoginPage from './pages/login/login';
+import { Toaster } from 'react-hot-toast';
 
 
 
@@ -10,15 +12,17 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Routes path="/*">
-        <Route path="/testing" element = {<Testing/>}/>
-        <Route path ="/admin/*" element={<AdminPage/>}/>
-        {/*'admin/*' මෙසේ යොදා ඇත්තෙ admin ලෙස url එකේ සදහන් කර එතනින් පස්සෙ ගහන
-        ඔනෑම අදාල නැති url වලින් එන අයට මුලින්ම admin page එක පෙන්නීම */}
-        
-        <Route path = "/*" element={<HomePage/>}/> {/* home එකේ url එකෙන් පස්සෙ ගහන අනිත් ඕනෑම අදාල නැති url වලින් එන අයට මුලින්ම home page එක පෙන්නීම*/}
+      <Toaster position='top-right'/>
+        <Routes path="/*">
+          <Route path="/testing" element = {<Testing/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path ="/admin/*" element={<AdminPage/>}/>
+          {/*'admin/*' මෙසේ යොදා ඇත්තෙ admin ලෙස url එකේ සදහන් කර එතනින් පස්සෙ ගහන
+          ඔනෑම අදාල නැති url වලින් එන අයට මුලින්ම admin page එක පෙන්නීම */}
+          
+          <Route path = "/*" element={<HomePage/>}/> {/* home එකේ url එකෙන් පස්සෙ ගහන අනිත් ඕනෑම අදාල නැති url වලින් එන අයට මුලින්ම home page එක පෙන්නීම*/}
 
-      </Routes>
+        </Routes>
   </BrowserRouter>
   )
 
--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,7 +1,47 @@
-export default function (){
+import {useState} from "react";
+export default function Testing(){
+    const[count,setCount] = useState(0);
+    const [item,setItemName] = useState("Coconut");
+
+   
     return(
-        <div>
-            <h1>Testing</h1>
+        
+        <div className = "w-full h-screen flex flex-col justify-center items-center">
+            <h1 className="text-9xl text-blue-700">{count} {item}</h1>
+            <button className="bg-black text-3xl text-white rounded-lg p-1" onClick={()=>{
+                const newCount = count + 1
+                setCount(newCount)
+                       
+            }
+            }>
+                click
+                
+            </button>
+            <div className=" w-full flex justify-evenly items-center p-4 ">
+                <button className="bg-black text-3xl text-white rounded-lg p-2" onClick={()=>{
+                    setItemName("Coconut")
+                }}>
+                    coconut
+                </button>
+                <button className="bg-black text-3xl text-white rounded-lg p-2"
+                onClick={()=>{
+                    setItemName("Orange")
+                }}>
+                    Orange
+                </button>
+                <button className="bg-black text-3xl text-white rounded-lg p-2" 
+                onClick={()=>{
+                    setItemName("Apple")
+                }}>
+                    Apple
+                </button>
+                <button className="bg-black text-3xl text-white rounded-lg p-2"
+                onClick={()=>{
+                    setItemName("Banana")
+                }}>
+                    Banana
+                </button>
+            </div>
         </div>
     )
 }
